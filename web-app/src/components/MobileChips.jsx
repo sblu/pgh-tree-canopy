2c40a778ba95cef161b5ce239857d9b5aba1e625
sblu <scottbluman@gmail.com>
2026-04-11 00:43:57 -0400

    Restore missing CSS: tree popup, street view modal, location dot; fix chip label

--- web-app/src/components/MobileChips.jsx
+++ web-app/src/components/MobileChips.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BOUNDARY_LAYERS, COLOR_METHODS } from '../config/layers'
 
 const CHIP_LABELS = {
-  neighborhoods:   'Neighborhoods',
+  neighborhoods:   'Hoods',
   city_council:    'City Council',
   county_council:  'County Council',
   parks_municipal: 'City Parks',
